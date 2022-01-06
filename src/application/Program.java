441b8ed7ed354e11ebd2dd396f7cc63359d58f20
Carlos <carlosportella16@hotmail.com>
2022-01-06 11:28:04 -0300

    Implementing possible moves of King

--- src/application/Program.java
+++ src/application/Program.java
@@ -28,6 +28,7 @@ public class Program {
 				UI.clearScreen();
 				UI.printBoard(chessMatch.getPieces(), possibleMoves);	
 				System.out.println();
+				System.out.println();
 				System.out.print("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);
 
